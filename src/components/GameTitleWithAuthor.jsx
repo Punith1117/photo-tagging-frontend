97cc7e6ba4c2a5e5bae34a2d6f4c39b7caee7777
Punith1117 <punith11172006@gmail.com>
2025-07-19 23:15:23 +0530

    feat(GameTitleWithAuthor): add new component with game title and author

---
+++ src/components/GameTitleWithAuthor.jsx
@@ -0,0 +1,10 @@
+const GameTitleWithAuthor = () => {
+    return (
+        <div>
+            <h1>Photo Tagging</h1>
+            <a href="https://github.com/Punith1117/photo-tagging-frontend">By Punith1117</a>
+        </div>
+    )
+}
+
+export default GameTitleWithAuthor
