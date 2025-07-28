56b44afaea039cd8455e4fb1fd86f0e61a839221
Punith1117 <punith11172006@gmail.com>
2025-07-28 20:03:26 +0530

    feat(TimeTaken): add new component to render time taken by the player

---
+++ src/components/TimeTaken.jsx
@@ -0,0 +1,32 @@
+import { use, useEffect, useState } from "react"
+import Loading from "./Loading"
+import { getTimeTakenByPlayer } from "../apiQueries"
+
+const TimeTaken = ({ userCreated }) => {
+    const [isLoading, setIsLoading] = useState(true)
+    const [timeTaken, setTimeTaken] = useState(null)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const timeTaken = await getTimeTakenByPlayer()
+            setTimeTaken(timeTaken)
+            setIsLoading(false)
+        }
+        if (userCreated == true)
+            fetchData()
+    }, [userCreated])
+
+    return (
+        <div>
+            <h2>Time Taken by you:</h2>
+            { (isLoading == true) ? 
+                <Loading /> : 
+                    (timeTaken == null) ? 
+                        <p>- seconds</p> : 
+                            <p>{timeTaken} seconds</p>
+            } 
+        </div>
+    )
+}
+
+export default TimeTaken
