3b6a658980e05f58233a247b99b65eedf3002c51
Punith1117 <punith11172006@gmail.com>
2025-07-28 19:46:54 +0530

    test(TimeTaken): add initial tests to a new component
    The test checks if the component renders loading and time taken by the player accordingly

---
+++ src/tests/TimeTaken.test.jsx
@@ -0,0 +1,43 @@
+import { render, screen } from "@testing-library/react";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import TimeTaken from "../components/TimeTaken";
+import '@testing-library/jest-dom';
+
+vi.mock('../apiQueries', () => ({
+    getTimeTakenByPlayer: vi.fn()
+}))
+import { getTimeTakenByPlayer } from "../apiQueries";
+
+vi.mock('../components/Loading', () => ({
+    default: () => <p>loading...</p>
+}))
+
+describe('TimeTaken component', () => {
+    beforeEach(() => {
+        vi.clearAllMocks()
+    })
+
+    it('exists', async () => {
+        render(<TimeTaken userCreated={true}/>)
+        expect(await screen.findByRole('heading')).toHaveTextContent(/time taken by you:/i)
+    })
+    it('renders loading component when user is not yet created', () => {
+        render(<TimeTaken userCreated={false}/>)
+        expect(screen.getByText(/loading.../i)).toBeInTheDocument()
+    })
+    it('renders loading component when data is being fetched when user is created', async () => {
+        getTimeTakenByPlayer.mockImplementation(() => new Promise(() => {}))
+        render(<TimeTaken userCreated={true} />)
+        expect(await screen.findByText(/loading.../i)).toBeInTheDocument()
+    })
+    it('renders result in the correct format when time taken is null', async () => {
+        getTimeTakenByPlayer.mockResolvedValue(null)
+        render(<TimeTaken userCreated={true} />)
+        expect(await screen.findByText(/- seconds/i)).toBeInTheDocument()
+    })
+    it('renders result in the correct format when time taken is not null', async () => {
+        getTimeTakenByPlayer.mockResolvedValue(20)
+        render(<TimeTaken userCreated={true} />)
+        expect(await screen.findByText(/20 seconds/i)).toBeInTheDocument()
+    })
+})
