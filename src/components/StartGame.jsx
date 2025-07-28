582c42ef280a251b0a94e77d368bc27c9fd79688
Punith1117 <punith11172006@gmail.com>
2025-07-28 23:59:17 +0530

    feat(StartGame): add new component

---
+++ src/components/StartGame.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+import Loading from "./Loading"
+
+const StartGame = ({userCreated}) => {
+    return (
+        <div>
+            (userCreated == true) ?
+                <Link to='/game'>Start</Link> :
+                    <span>{<Loading />}</span>
+        </div>
+    )
+}
+
+export default StartGame
