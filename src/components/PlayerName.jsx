be46a009e63fd88ecf1cd809cb6f9c49813d4745
Punith1117 <punith11172006@gmail.com>
2025-07-28 17:34:19 +0530

    feat(PlayerName): add new component to display player name

---
+++ src/components/PlayerName.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react"
+import Loading from "./Loading"
+import { getPlayerNameFromToken } from "../utilities"
+
+const PlayerName = ({ userCreated }) => {
+    const [playerName, setPlayerName] = useState(null)
+
+    useEffect(() => {
+        if (userCreated)
+            setPlayerName(getPlayerNameFromToken)
+    }, [userCreated])
+
+    return (
+        <div>
+            <h2>Player Name</h2>
+            {
+                (playerName == null) ? <Loading /> : <p>{playerName}</p>
+            }
+        </div>
+    )
+}
+
+export default PlayerName
