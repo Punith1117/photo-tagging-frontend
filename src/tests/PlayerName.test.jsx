af842c52cd866dac45cf5bf76273580632bc6113
Punith1117 <punith11172006@gmail.com>
2025-07-28 17:23:11 +0530

    test(PlayerName): add initial tests
    The tests check if the component exists, renders loading component and finally player name accordingly

---
+++ src/tests/PlayerName.test.jsx
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+import PlayerName from "../components/PlayerName";
+import '@testing-library/jest-dom';
+
+vi.mock('../utilities', () => ({
+    getPlayerNameFromToken: vi.fn()
+}))
+
+vi.mock('../components/Loading', () => ({
+    default: () => <p>loading...</p>
+}))
+
+import { getPlayerNameFromToken } from "../utilities";
+
+describe('PlayerName component', () => {
+    it('exists', () => {
+        render(<PlayerName userCreated={false} />)
+        expect(screen.getByText(/player name/i)).toBeInTheDocument()
+    })
+    it('renders loading component when userCreated is false', () => {
+        render(<PlayerName userCreated={false} />)
+        expect(screen.getByText(/loading.../i)).toBeInTheDocument()
+    })
+    it('renders player name when userCreated is true', async () => {
+        getPlayerNameFromToken.mockReturnValue('Punith')
+        render(<PlayerName userCreated={true} />)
+        expect(await screen.getByText('Punith')).toBeInTheDocument()
+    })
+})
