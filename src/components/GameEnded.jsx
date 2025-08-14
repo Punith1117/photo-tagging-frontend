c28b1450f79bec917a9362aa0c5f01bd74b0b1df
Punith1117 <punith11172006@gmail.com>
2025-08-14 15:09:38 +0530

    feat(GameEnded): add new component with basic styling

---
+++ src/components/GameEnded.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom"
+import styled from "styled-components"
+
+const StyledGameEnded = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    background-color: black;
+`
+
+const GameEnded = ({refresh, setRefresh}) => {
+    return (
+        <StyledGameEnded>
+            <h2>Game Ended</h2>
+            <button onClick={() => setRefresh(refresh+1)}>Play again</button>
+            <Link to={'/'}>Home</Link>
+        </StyledGameEnded>
+    )
+}
+
+export default GameEnded
