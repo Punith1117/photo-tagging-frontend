a62f5c432236cca75fe30c6c48dfdd7793092186
Punith1117 <punith11172006@gmail.com>
2025-08-14 19:25:34 +0530

    feat(ObjectsToFind): add new component

---
+++ src/components/ObjectsToFind.jsx
@@ -0,0 +1,47 @@
+import styled from "styled-components"
+import Loading from "./Loading"
+
+const StyledObjectsToFind = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .objects {
+        display: flex;
+        gap: 10px;
+    }
+
+    .object-wrapper {
+        width: 5rem;
+        height: auto;
+
+        img {
+            width: 100%;
+            height: 100%;
+            display: block;
+        }
+    }
+`
+
+const ObjectsToFind = ({ objects }) => {
+    return (
+        <StyledObjectsToFind>
+            <h2>Objects to find</h2>
+            <div className="objects">
+                {
+                    (objects === null) ?
+                        <Loading />
+                    :
+                    objects.map(object => (
+                        <div className="object-wrapper">
+                            <img src={`/objects/${object.id}.jpeg`} alt={`object ${object.id}`} />
+                        </div>
+                    ))
+                }
+            </div>
+        </StyledObjectsToFind>
+    )
+}
+
+export default ObjectsToFind
