43913dd16e0d060427957cdeb7c8e49f7de9f689
Punith1117 <punith11172006@gmail.com>
2025-08-14 17:09:07 +0530

    feat(routing): add '/game' route with 'Game' component

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Index from "./components/Index";
 import ErrorPage from "./components/ErrorPage";
+import Game from "./components/Game";
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Index />
+  },
+  {
+    path: '/game',
+    element: <Game />
   },
   {
     path: '*',
