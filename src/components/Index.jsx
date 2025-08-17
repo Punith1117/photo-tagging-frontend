5be83a29cea6811e528c064ee0fc1a84d082871a
Punith1117 <punith11172006@gmail.com>
2025-08-17 22:13:55 +0530

    fix(Index): set userCreated to true after creating new user

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -15,6 +15,7 @@ const Index = () => {
         const createUser = async () => {
             const token = await getNewPlayerToken()
             savePlayerTokenToStorage(token)
+            setUserCreated(true)
         }
         if (getPlayerTokenFromStorage() == null)
             createUser()
