56992acb5c37496088802e2360a6d11230f7d757
Punith1117 <punith11172006@gmail.com>
2025-08-17 22:16:02 +0530

    feat(utilities): add functions to get and save player token in local storage

--- src/utilities.js
+++ src/utilities.js
@@ -1,8 +1,22 @@
 import { jwtDecode } from "jwt-decode"
 
-export const getPlayerNameFromToken = () => {
+const getPlayerNameFromToken = () => {
     const jwt = localStorage.getItem('player-token')
     if (!jwt) return null
     const decoded = jwtDecode(jwt)
     return decoded.playerName
+}
+
+const savePlayerTokenToStorage = (token) => {
+    localStorage.setItem('player-token', token)
+}
+
+const getPlayerTokenFromStorage = () => {
+    return localStorage.getItem('player-token')
+}
+
+export {
+    getPlayerNameFromToken,
+    savePlayerTokenToStorage,
+    getPlayerTokenFromStorage
 }
