39dc008a53ce20db1a43f99f6fe1cd44404d0ae0
Punith1117 <punith11172006@gmail.com>
2025-08-18 23:26:44 +0530

    style(GameEnded): add complete styling to component

--- src/components/GameEnded.jsx
+++ src/components/GameEnded.jsx
@@ -1,10 +1,22 @@
 import styled from "styled-components"
 
 const StyledGameEnded = styled.div`
-    position: absolute;
-    top: 50%;
+    position: fixed;
+    top: 25rem;
     left: 50%;
+    transform: translateX(-50%);
     background-color: black;
+    border-radius: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 2rem;
+
+    h2 {
+        padding: 0;
+        margin: 0;
+    }
 `
 
 const GameEnded = ({refresh, setRefresh, gameEndedMessage}) => {
