2dd570dfa8f83239cb538b4ea7c69eaa167e0afb
Punith1117 <punith11172006@gmail.com>
2025-08-18 22:38:45 +0530

    style(Notifications): apply complete styling to component

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -1,11 +1,24 @@
+import styled from "styled-components"
+
+const StyledNotifications = styled.div`
+    position: fixed;
+    right: 0;
+    top: 0;
+    margin: 3rem;
+    font-size: 1.2rem;
+    z-index: 1000;
+    color: white;
+    font-weight: 800;
+    text-shadow: 3px 3px 1px black;
+`
 
 const Notifications = ({ messages }) => {
     return (
-        <div>
+        <StyledNotifications>
             {messages.map(message => (
                 <p key={message.id} message={message}>{message.text}</p>
             ))}
-        </div>
+        </StyledNotifications>
     )
 }
 
