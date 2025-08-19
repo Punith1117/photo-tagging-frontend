a74062600d02ac5d070cfe901428478ac57ea10b
Punith1117 <punith11172006@gmail.com>
2025-08-19 12:42:04 +0530

    style(Leaderboard): add complete styling to component and modify index.css

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,7 +1,42 @@
 import { useEffect, useState } from "react"
 import Loading from "./Loading"
 import { getLeaderboard } from "../apiQueries"
+import styled from "styled-components"
 
+const StyledLeaderboard = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    margin: 2vw;
+    display: flex;
+    flex-direction: column;
+    place-items: center;
+    background-color: #011969;
+    padding: 1vw;
+    border-radius: 2rem;
+
+    h2 {
+        margin: 0;
+    }
+    table {
+        padding: 5px;
+        * {
+            border: 1px solid white;
+            padding: 5px;
+        }
+        
+        th {
+            background-color: #b83737;
+
+            p {
+                font-size: 13px;
+                padding: 0;
+                border: 0;
+                margin: 0;
+            }
+        }
+    }
+`
 const Leaderboard = () => {
     const [players, setPlayers] = useState(null)
 
@@ -24,14 +59,14 @@ const Leaderboard = () => {
     )
     else {
         return (
-            <div>
+            <StyledLeaderboard>
                 <h2>Leaderboard</h2>
                 <table>
                 <thead>
                     <tr>
                         <th>Rank</th>
                         <th>Player name</th>
-                        <th>Time taken in seconds</th>
+                        <th>Time taken <p>in seconds</p></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -46,7 +81,7 @@ const Leaderboard = () => {
                     }
                 </tbody>
                 </table>
-            </div>
+            </StyledLeaderboard>
         )
     }
 }
